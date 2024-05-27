990dc0420975dbc923c48732e943cdcb5617a498
lakshmi adriana prieto ortega <lakshmiadriana.prietoor@cbtis15.edu.mx>
2024-05-27 16:29:28 -0600

    tienda 27 mayo

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/tienda/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda";
+const CHARSET = "charset=utf8";
+const TITLE = "Tienda online";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+?>
---
+++ Models/PrincipalModels.php
@@ -0,0 +1,20 @@
+<?php
+class PrincipalModel extends Query{
+ 
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function getProducto($id_producto)
+    {
+        $sql = "SELECT p.*, c.categoria FROM productos p INNER JOIN categorias c ON p.id_categoria = c.id WHERE p.id = $id_producto";
+        return $this->selectAll($sql);
+    }
+    public function getProductos() {
+        $sql = "SELECT * FROM productos";
+        return $this->selectAll($sql);
+    }
+}
+ 
+?>
