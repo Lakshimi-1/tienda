7d174a5a57f1e1c72191cad3a3e3ef6dc9e8ca88
Lakshmi Adriana Prieto Ortega <lakshmiadriana.prietoor@cbtis15.edu.mx>
2024-05-31 22:50:51 -0600

    a

--- Views/principal/detail.php
+++ Views/principal/detail.php
@@ -189,7 +189,7 @@
                             <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
                                 <ul class="list-unstyled">
                                     <li><a class="btn btn-success text-white" href="shop-single.html"><i class="fas fa-heart"></i></a></li>
-                                    <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="fas fa-eye"></i></a></li>
+                                    <li><a class="btn btn-success text-white mt-2" href="<?php echo BASE_URL . 'principal/detail/' . $relacion['id']; ?>"><i class="fas fa-eye"></i></a></li>
                                     <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="fas fa-cart-plus"></i></a></li>
                                 </ul>
                             </div>
