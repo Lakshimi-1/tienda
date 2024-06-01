51a238ebdfa5710ebcb697f4cc56b3a5322c8bf1
lakshmi adriana prieto ortega <lakshmiadriana.prietoor@cbtis15.edu.mx>
2024-05-31 19:48:28 -0600

    a

--- Models/PrincipalModel.php
+++ Models/PrincipalModel.php
@@ -32,6 +32,12 @@ class PrincipalModel extends Query{
         $sql = "SELECT COUNT(*) AS total FROM productos WHERE id_categoria = $id_categoria";
         return $this->select($sql);
     }
+    //productos relacionados aleatorios
+    public function getAleatorios($id_categoria)
+    {
+        $sql = "SELECT * FROM productos  WHERE id_categoria = $id_categoria ORDER BY RAND()  LIMIT 20";
+        return $this->selectAll($sql);
+    }
 
 }
  
