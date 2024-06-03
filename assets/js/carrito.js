29dacb0f98d8dcec58255131af054b1f148b3d8f
Lakshmi Adriana Prieto Ortega <lakshmiadriana.prietoor@cbtis15.edu.mx>
2024-06-02 23:50:43 -0600

    a

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,6 +1,8 @@
 const btnAddDeseo = document.querySelectorAll('.btnAddDeseo');
+const btnDeseo = document.querySelector('#btnCantidadDeseo');
 let listaDeseo = [];
 document.addEventListener('DOMContentLoaded',function () {
+    cantidadDeseo();
     for (let i = 0; i < btnAddDeseo.length; i++) {
         btnAddDeseo[i].addEventListener('click', function () {
             let idProducto = btnAddDeseo[i].getAttribute('prod');
@@ -14,4 +16,15 @@ function agregarDeseo(idProducto) {
         "cantidad" : 1
     })
     localStorage.setItem('listaDeseo', JSON.stringify(listaDeseo));
+    cantidadDeseo();
+}
+function cantidadDeseo() {
+    let listas = JSON.parse(localStorage.getItem('listaDeseo'));
+    if (listas != null) {
+        btnDeseo.textContent = listas.length;
+    } else {
+        btnDeseo.textContent = 0;
+    }
+    console.log(listas);
+    btnDeseo.textContent = listas.length;
 }
