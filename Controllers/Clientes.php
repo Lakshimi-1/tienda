28145bb61f0aaa77e8a62bf775237925975cde1b
lakshmi adriana prieto ortega <lakshmiadriana.prietoor@cbtis15.edu.mx>
2024-06-06 17:06:12 -0600

    aq

--- Controllers/Clientes.php
+++ Controllers/Clientes.php
@@ -10,5 +10,13 @@ class Clientes extends Controller
         $data['title'] = 'Tu Perfil';
         $this->views->getView('principal', "perfil", $data);
     }
-    
+    public function registroDirecto()
+    {
+        if (isset($_POST['nombre']) && isset($_POST['clave'])) {
+            $nombre = $_POST['nombre'];
+            $correo = $_POST['correo'];
+            $clave = $_POST['clave'];
+            $data = $this->model->registroDirecto($nombre, $nombre, $clave);
+        }
+    }
 }
--- assets/js/login.js
+++ assets/js/login.js
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
         http.send(formData);
         http.onreadystatechange = function () {
           if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
             const res = JSON.parse(this.responseText);
           }
         };
